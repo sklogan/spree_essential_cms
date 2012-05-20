0401b4acf41892a275bde1d6787a80d632d9390e
Bounmy Stephane <stephanebounmy@gmail.com>
2012-05-20 23:50:55 +0200

    moved spree::possible_page to real model so we can override / eval it

---
+++ app/models/spree/possible_page.rb
@@ -0,0 +1,8 @@
+module Spree
+  class PossiblePage
+    def self.matches?(request)
+      return false if request.fullpath =~ /(^\/+(admin|account|cart|checkout|content|login|pg\/|orders|products|s\/|session|signup|shipments|states|t\/|tax_categories|user)+)/
+      !Page.active.find_by_path(request.fullpath).nil?
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,18 @@
-class Spree::PossiblePage
-  def self.matches?(request) 
-    return false if request.fullpath =~ /(^\/+(admin|account|cart|checkout|content|login|pg\/|orders|products|s\/|session|signup|shipments|states|t\/|tax_categories|user)+)/
-    !Spree::Page.active.find_by_path(request.fullpath).nil?
-  end
-end
-
 Spree::Core::Engine.routes.append do
-  
+
   namespace :admin do
-    
+
     resources :pages, :constraints => { :id => /.*/ } do
       collection do
         post :update_positions
       end
-    
+
       resources :contents do
         collection do
           post :update_positions
         end
       end
-    
+
       resources :images, :controller => "page_images" do
         collection do
           post :update_positions
@@ -28,9 +21,9 @@ Spree::Core::Engine.routes.append do
     end
 
   end
-  
+
   constraints(Spree::PossiblePage) do
     get '(:page_path)', :to => 'pages#show', :page_path => /.*/, :as => :page
   end
-  
+
 end
