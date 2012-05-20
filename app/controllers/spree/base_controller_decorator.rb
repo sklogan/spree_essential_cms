85e530092b776257c7f67e02701962d4b38a4970
Bounmy Stephane <stephanebounmy@gmail.com>
2012-05-21 01:16:30 +0200

    fix engine bug with //

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -2,11 +2,14 @@ Spree::BaseController.class_eval do
 
   before_filter :get_pages
   helper_method :current_page
-  
+
+  # request.fullpath returns extra '/'
+  # Example: //my_path
+  # dirty fix: manual gsub for the moment
   def current_page
-    @page ||= Spree::Page.find_by_path(request.fullpath)
+    @page ||= Spree::Page.find_by_path(request.fullpath.gsub('//', '/'))
   end
-  
+
   def get_pages
     return if request.path =~ /^\/+admin/
     @pages ||= Spree::Page.visible.order(:position).all
