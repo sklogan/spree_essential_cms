751fb9eb413b86bee03d3079158a110f31970542
Bounmy Stephane <stephanebounmy@gmail.com>
2012-05-21 00:38:13 +0200

    bug with engine, fullpath starts with //

--- app/models/spree/possible_page.rb
+++ app/models/spree/possible_page.rb
@@ -1,7 +1,7 @@
 module Spree
   class PossiblePage
     def self.matches?(request)
-      return false if request.fullpath =~ /(^\/+(admin|account|cart|checkout|content|login|pg\/|orders|products|s\/|session|signup|shipments|states|tax_categories|user)+)/
+      return false if request.fullpath =~ /(^\/\/+(admin|account|cart|checkout|content|login|pg\/|orders|products|s\/|session|signup|shipments|states|tax_categories|user)+)/
       !Page.active.find_by_path(request.fullpath).nil?
     end
   end
